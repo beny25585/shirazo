1488d7cd5cc0dedb3593da033c5ab0f8ef117781
beny <binyamin25585@gmail.com>
2025-06-21 04:45:15 +0300

    fix robots.txt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Mail, MessageCircle } from "lucide-react";
-import ProjectsCard, { Project } from "../components/ui/ProjectsCard";
+import ProjectsCard from "../components/ui/ProjectsCard";
 import { projects } from "@/data/projects";
 
 export default function Component() {
